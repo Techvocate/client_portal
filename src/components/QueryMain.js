14dbb6e5bc2efd357a9d1b8174867542fb5d4cde
devesh mishra <E22CSEU0766@bennett.edu.in>
2023-09-21 20:29:41 +0530

    queryMain changes part3

--- src/components/QueryMain.js
+++ src/components/QueryMain.js
@@ -16,6 +16,14 @@ function QueryMain() {
           <img className="chatImg" src={gptImgLogo} alt=''/>
           <p className='txt'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab dolorum optio voluptate a error, ut libero cumque saepe velit, quis nam asperiores dolor? Minima quos cupiditate rerum obcaecati beatae quibusdam nobis. Mollitia, autem. Dicta laudantium nisi aliquam distinctio dolor vel cupiditate culpa animi quaerat, maiores deserunt. Exercitationem assumenda nobis, optio veniam, dolor neque ratione suscipit placeat ullam ipsum, quas modi. Enim sint earum dignissimos excepturi, libero fugiat labore neque, harum corporis cum fugit. Quia, vel corporis est laboriosam ipsam ducimus. Natus repudiandae dolorum harum dolore doloremque temporibus aliquam nostrum accusantium laboriosam numquam, impedit amet corporis consequatur vel labore vero minus.</p>
         </div>
+        <div className='chat1'>
+          <img className='chatImg' src={userIcon} alt=''/>
+          <p className='txt'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+        <div className='chat1 bot'>
+          <img className="chatImg" src={gptImgLogo} alt=''/>
+          <p className='txt'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab dolorum optio voluptate a error, ut libero cumque saepe velit, quis nam asperiores dolor? Minima quos cupiditate rerum obcaecati beatae quibusdam nobis. Mollitia, autem. Dicta laudantium nisi aliquam distinctio dolor vel cupiditate culpa animi quaerat, maiores deserunt. Exercitationem assumenda nobis, optio veniam, dolor neque ratione suscipit placeat ullam ipsum, quas modi. Enim sint earum dignissimos excepturi, libero fugiat labore neque, harum corporis cum fugit. Quia, vel corporis est laboriosam ipsam ducimus. Natus repudiandae dolorum harum dolore doloremque temporibus aliquam nostrum accusantium laboriosam numquam, impedit amet corporis consequatur vel labore vero minus.</p>
+        </div>
       </div>
       <div className='chatFooter'>
         <div className='inp'>
