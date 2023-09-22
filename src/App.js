41c145ba6acd1ec6010b4b953081de50f5712a0b
Anuj Srivastava <118746163+anuj1o0@users.noreply.github.com>
2023-09-22 23:22:11 +0530

    react-route

--- src/App.js
+++ src/App.js
@@ -4,15 +4,21 @@ import Navbar from './components/Navbar';
 import Homepage from './components/HomePage';
 import UseCases from './components/UseCases';
 import QueryPage from './components/QueryPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Homepage/>
-      <QueryPage/>
+      
+      <Router>
+        
+        <Routes>
+          <Route path="/" element={<Homepage/>}/>
+          <Route path="/querypage" element={<QueryPage/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
 
-export default App;
+export default App;
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,23 +1,36 @@
 // src/components/HomePage.js
-import './Homepage.css';
-import React from 'react';
-import {motion} from 'framer-motion';
+import "./Homepage.css";
+import React from "react";
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
+import Navbar from "./Navbar";
 
 const HomePage = () => {
   return (
     <div className="home-page">
-        <div className="content">
-        <p className="main-title">Streamline<br/> Your Legal Processes</p>
+      <Navbar/>
+      <div className="content">
+        <p className="main-title">
+          Streamline
+          <br /> Your Legal Processes
+        </p>
         <p className="sub-heading">Effortless Document Management</p>
         <p className="description">
-          Effortlessly generate, customize, and manage a wide range of legal documents tailored to your specific needs.
+          Effortlessly generate, customize, and manage a wide range of legal
+          documents tailored to your specific needs.
         </p>
-        <motion.div 
-        whileInView={{opacity: 1}}
-        whileHover={{scale: 1.1}}
-        transition={{duration: 0.5,type: 'tween'}}
-        className="app__profile-item"
-        ><button className="try-now-button">Try Now <span style={{fontSize:"20px"}}>→</span></button></motion.div>
+        <motion.div
+          whileInView={{ opacity: 1 }}
+          whileHover={{ scale: 1.1 }}
+          transition={{ duration: 0.5, type: "tween" }}
+          className="app__profile-item"
+        >
+          <Link to="/querypage">
+            <button className="try-now-button">
+              Try Now <span style={{ fontSize: "20px" }}>→</span>
+            </button>
+          </Link>
+        </motion.div>
       </div>
       <video autoPlay loop muted className="video-bg">
         <source src="\pexels_videos_1851190 (2160p).mp4" type="video/mp4" />
@@ -25,6 +38,6 @@ const HomePage = () => {
       </video>
     </div>
   );
-}
+};
 
 export default HomePage;
