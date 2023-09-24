ded5043c597850bb7e4db39f4b02a5262bec7136
Anuj Srivastava <118746163+anuj1o0@users.noreply.github.com>
2023-09-24 15:49:58 +0530

    legal Advice changes

--- src/components/LegalAdvice.js
+++ src/components/LegalAdvice.js
@@ -3,7 +3,7 @@ import "./Homepage.css";
 import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
-import Navbar from "./Navbar";
+import Navbar from "./LegalAdvice.css";
 
 const LegalAdvice = () => {
   
