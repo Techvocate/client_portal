9a6f9e37f271de34c996842d729465f9b9b2a123
Anuj Srivastava <118746163+anuj1o0@users.noreply.github.com>
2023-11-26 12:05:11 +0530

    navbar changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,51 +1,56 @@
 import React from 'react';
 import './Navbar.css';
+import gptlogo from '../assets/integrity2.png';
 import { Link } from 'react-router-dom';
-// import { useEffect,useContext} from 'react';
-// import {UserContext} from "../UserContext";
+import { useEffect,useContext} from 'react';
+import {UserContext} from "../UserContext";
 
 const Navbar = () => {
-  // const {setUserInfo,userInfo} = useContext(UserContext);
-  // useEffect(() => {
-  //   fetch('http://localhost:4000/profile', {
-  //     credentials: 'include',
-  //   }).then(response => {
-  //     response.json().then(userInfo => {
-  //       setUserInfo(userInfo);
-  //     });
-  //   });
-  // }, []);
+  const {setUserInfo,userInfo} = useContext(UserContext);
+  useEffect(() => {
+    fetch('http://localhost:4000/profile', {
+      credentials: 'include',
+    }).then(response => {
+      response.json().then(userInfo => {
+        setUserInfo(userInfo);
+      });
+    });
+  }, []);
 
-  // function logout() {
-  //   fetch('http://localhost:4000/logout', {
-  //     credentials: 'include',
-  //     method: 'POST',
-  //   });
-  //   setUserInfo(null);
-  // }
+  function logout() {
+    fetch('http://localhost:4000/logout', {
+      credentials: 'include',
+      method: 'POST',
+    });
+    setUserInfo(null);
+  }
 
-  // const username = userInfo?.username;
+  const username = userInfo?.username;
   return (
     <nav className="navbar1">
       <div className="navbar-container">
         <div className='navbar-title'>
+        <img src={gptlogo} alt='logo' className='logo'/>
             <p id='title'>LegalEase</p>
         </div>
         <ul className="navbar-links">
           <Link to={'/'}><li className="navbar-link">Home</li></Link>
           <Link to={'/legaladvice'}><li className="navbar-link">Legal Advice</li></Link>
           <li className="navbar-link">About</li>
+          
           {/* <li className="navbar-link">UseCases</li> */}
         </ul>
-        {/* {!username && <div className="navbar-auth">
+        
+        
+        { !username && <div className="navbar-auth">
           <Link><button className="auth-button">Login</button></Link>
           <Link to={'/signup'}><button className="auth-button">Signup</button></Link>
         </div>}
         {username && <div className="navbar-auth">
           <Link to={'/profile'}><button className="auth-button">{username}</button></Link>
           <button className="auth-button" onClick={logout}>Logout</button>
-        </div>} */}
-        {/* {username && (
+        </div>} 
+         {username && (
           <div className='navbar-auth'>
               <div>{username}</div>
               <div><Link className='auth-button' onClick={logout}>Logout</Link></div>
@@ -56,7 +61,7 @@ const Navbar = () => {
             <div eventKey={2}><Link className='auth-button' to={'/login'}>Login</Link></div>
             <div eventKey={2}><Link className='auth-button' to={'/signup'}>Signup</Link></div>
           </div>
-        )} */}
+        )} 
       </div>
     </nav>
   );
